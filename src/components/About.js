b31cb72e139cddc6f53203d14a8e5931cab3b4aa
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-03-20 16:07:21 +0000

    Removed code that shouldn't be in App.js and replaced it with Routes. Setup Routes for Home and About and linked them up.
    Changed the Start Now button to an About Us button which takes the user to an About Us page

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1>About Us Page</h1>
+      <a href='/' className='btn_1 rounded'>
+        Home
+      </a>
+    </div>
+  )
+}
+
+export default About
--- src/components/pages/LeftContent.js
+++ src/components/pages/LeftContent.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react'
 
 function LeftContent(props) {
   return (
     <div>
       <figure>
-        <img src={props.img} alt="" className="img-fluid" />
+        <img src={props.img} alt='' className='img-fluid' />
       </figure>
       <h2>{props.heading}</h2>
       <p>{props.subtext}</p>
-      <a href="#0" className="btn_1 rounded">
-        Start Now!
+      <a href='/about' className='btn_1 rounded'>
+        About Us
       </a>
     </div>
-  );
+  )
 }
 
-export default LeftContent;
+export default LeftContent
