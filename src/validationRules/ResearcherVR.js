b11c091a2af4a502580085a290fbab3a0af106b0
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-03-20 15:11:15 +0000

    Removed required property from OrcidID but left it commented incase it's required in the future

--- src/validationRules/ResearcherVR.js
+++ src/validationRules/ResearcherVR.js
@@ -4,28 +4,36 @@ export default function validateResearcher({
   school,
   otherSchool,
   careerStage,
+  orcidID,
 }) {
-  let errors = {};
+  let errors = {}
 
   if (!fullName) {
-    errors.fullName = "Required";
+    errors.fullName = 'Required'
   }
 
   if (!faculty) {
-    errors.faculty = "Required";
+    errors.faculty = 'Required'
   }
 
   if (!school) {
-    errors.school = "Required";
-  } else if (school === "other") {
+    errors.school = 'Required'
+  } else if (school === 'other') {
     if (!otherSchool) {
-      errors.otherSchool = "Required";
+      errors.otherSchool = 'Required'
     }
   }
 
   if (!careerStage) {
-    errors.careerStage = "Required";
+    errors.careerStage = 'Required'
   }
 
-  return errors;
+  // Uncomment to make OrcidID a required field
+  // if (!orcidID) {
+  //   errors.orcidID = 'Required'
+  // } else if (!/\d{4}-\d{4}-\d{4}-\d{4}/g.test(orcidID)) {
+  //   errors.orcidID = 'Must be a valid ORCID ID'
+  // }
+
+  return errors
 }
