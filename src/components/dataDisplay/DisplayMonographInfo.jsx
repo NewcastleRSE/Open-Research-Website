4787e13402a2b5bbdf1ad11c524b180a804ea91b
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-04-30 20:02:57 +0100

    Fixed issue with monograph embargo

--- src/components/dataDisplay/DisplayMonographInfo.jsx
+++ src/components/dataDisplay/DisplayMonographInfo.jsx
@@ -1,11 +1,12 @@
 const DisplayMonographInfo = ({ monographData, field }) => {
+  console.log(monographData);
   return monographData.map((monograph, index) => {
     return (
       <div key={index} className="Results__List">
         {field("Title", monograph.monographTitle)}
         {field("URL", monograph.monographURL)}
         {field("DOI", monograph.monographDOI)}
-        {field("Embargo", monograph.monographEmbargo)}
+        {field("Embargo", monograph.monographEmbargo ? "True" : "False")}
         {field("License", monograph.monographLicense)}
       </div>
     );
